1b116d262530f56adc91561bc2748e76b406fdfe
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2021-01-06 12:38:06 -0500

    button variant

--- client/components/Button/index.tsx
+++ client/components/Button/index.tsx
@@ -5,28 +5,48 @@ import { theme as t } from '../../styles/Theme';
 interface IButtonProps {
   text: string;
   to: string;
+  variant?: string;
 }
 
-const Btn = styled.a`
+interface IBtnProps {
+  variant?: string;
+}
+
+const Btn = styled.a<IBtnProps>`
   font-size: 1.4rem;
+  text-align: center;
   padding: 2rem 0;
   display: inline-block;
   width: 30rem;
   background-color: ${t.colors.primary};
   color: ${t.colors.white};
-  border: 1px solid ${t.colors.white};
+  border-radius: 0.5rem;
   cursor: pointer;
   transition: background-color 0.2s;
 
   &:hover {
     background-color: ${t.colors.darkPrimary};
   }
+
+  ${props => {
+    switch (props.variant) {
+      case 'black':
+        return `
+          box-shadow: 0 5px 10px -5px;
+          border: 1px solid ${t.colors.lightEbony};
+        `;
+      default:
+        return `
+          border: 1px solid ${t.colors.white}; 
+        `;
+    }
+  }}
 `;
 
-export default function Button({ text, to }: IButtonProps) {
+export default function Button({ text, to, variant }: IButtonProps) {
   return (
     <Link href={`/${to}`}>
-      <Btn>{text.toUpperCase()}</Btn>
+      <Btn variant={variant}>{text.toUpperCase()}</Btn>
     </Link>
   );
 }
