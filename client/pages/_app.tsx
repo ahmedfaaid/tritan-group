75be773a4cc8539a1b85ee1de6e5aabe37b62b76
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2021-02-09 15:23:24 -0500

    add fortawesome config

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,4 +1,7 @@
+import { config, library } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
 import { GlobalStyles } from '../styles/GlobalStyles';
+config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }) {
   return (
