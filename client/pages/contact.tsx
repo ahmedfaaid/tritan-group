8edd79654280d0ad0cf04991678a08202d14e34e
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2021-02-08 20:46:11 -0500

    add contact page

---
+++ client/pages/contact.tsx
@@ -0,0 +1,11 @@
+import Layout from '../components/layout';
+
+export default function Contact() {
+  return (
+    <Layout>
+      <div>
+        <h3>Contact</h3>
+      </div>
+    </Layout>
+  );
+}
