32d3bdfdb653cf89f2bf17fe6e2dedfff290a048
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2021-02-11 10:27:24 -0500

    add employers page

---
+++ client/pages/employers.tsx
@@ -0,0 +1,15 @@
+import ImageStrip from '../components/ImageStrip';
+import Layout from '../components/layout';
+
+export default function Employers() {
+  return (
+    <Layout page='Employers'>
+      <section>
+        <ImageStrip
+          image='alevision-co-fYyYz38bUkQ-unsplash.jpg'
+          text='Employers'
+        />
+      </section>
+    </Layout>
+  );
+}
