82c1e751fb02272bf4b96fca32d657d30579b3b5
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2021-02-11 09:06:16 -0500

    add talent subpages

---
+++ client/pages/talent/index.tsx
@@ -0,0 +1,9 @@
+import Layout from '../../components/layout';
+
+export default function Talent() {
+  return (
+    <Layout>
+      <h1>Talent</h1>
+    </Layout>
+  );
+}
---
+++ client/pages/talent/jobs.tsx
@@ -0,0 +1,9 @@
+import Layout from '../../components/layout';
+
+export default function Jobs() {
+  return (
+    <Layout>
+      <h1>Jobs Board</h1>
+    </Layout>
+  );
+}
