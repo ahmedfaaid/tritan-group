98dd0456238f3e7f5d2f43ad959f5cffcbd0e6c4
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2021-02-11 10:07:36 -0500

    jobs subpage image strips

--- client/pages/talent/index.tsx
+++ client/pages/talent/index.tsx
@@ -1,9 +1,13 @@
 import Layout from '../../components/layout';
+import ImageStrip from '../../components/ImageStrip';
 
 export default function Talent() {
   return (
     <Layout page='Talent'>
-      <h1>Talent</h1>
+      <ImageStrip
+        image='javier-allegue-barros-i5Kx0P8A0d4-unsplash.jpg'
+        text='Talent'
+      />
     </Layout>
   );
 }
--- client/pages/talent/jobs.tsx
+++ client/pages/talent/jobs.tsx
@@ -1,9 +1,13 @@
 import Layout from '../../components/layout';
+import ImageStrip from '../../components/ImageStrip';
 
 export default function Jobs() {
   return (
     <Layout page='Jobs Board'>
-      <h1>Jobs Board</h1>
+      <ImageStrip
+        image='alvaro-reyes-6avV9oeHxfo-unsplash.jpg'
+        text='Job Board'
+      />
     </Layout>
   );
 }
