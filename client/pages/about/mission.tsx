37b2939de7d1ecf933d2730ee24d351c3bd4e4b9
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2021-02-20 16:45:05 -0500

    values content

--- client/pages/about/mission.tsx
+++ client/pages/about/mission.tsx
@@ -115,22 +115,39 @@ const Cards = styled.div<ICardsProps>`
 export default function Mission() {
   const values = [
     {
+      value: 'Smart Work',
+      text:
+        'Finding and delivering the most effective and efficient results without sacrificing quality or integrity.',
+      image: 'olav-ahrens-rotne-4Ennrbj1svk-unsplash.jpg'
+    },
+    {
       value: 'Professionalism',
       text:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla finibus, erat et placerat congue, ipsum quam feugiat sapien, at vestibulum quam dui a odio.',
+        'Using our in-depth knowledge, industry knowledge and expertise to serve you. We are industry expert, and We take ownership of our work.',
       image: 'hunters-race-MYbhN8KaaEc-unsplash.jpg'
     },
     {
-      value: 'Experts',
-      text:
-        'Donec eget neque at tellus ornare dapibus. Morbi mollis ultricies enim, at bibendum justo. Proin condimentum eget justo at interdum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      value: 'Responsiveness',
+      text: 'We take ownership of our work and deliver timely results.',
       image: 'alizee-baudez-npJkoTc0P8s-unsplash.jpg'
     },
     {
       value: 'Customer Focus',
       text:
-        'Sed pretium magna est, in cursus felis suscipit sed. Fusce faucibus est leo, pretium pulvinar mi facilisis nec. Phasellus molestie viverra nunc, sed tincidunt magna aliquam ac.',
+        'Our solutions are customized for each individual client, candidate and situation',
       image: 'blake-wisz-0mZIT-EcH5k-unsplash.jpg'
+    },
+    {
+      value: 'Integrity',
+      text:
+        'We do not compromise our values, our clients, or candidates just to close a deal. We believe in honest and open communication that include speaking the fact-based truths, even when it’s hard to hear it.',
+      image: 'brett-jordan-3h6Q-9Y0LqI-unsplash.jpg'
+    },
+    {
+      value: 'Respect',
+      text:
+        'We value our clients and candidates trust and confidence. We will safeguard your privacy and not waste your time.',
+      image: 'tiago-felipe-ferreira-saJkxOZXPsk-unsplash.jpg'
     }
   ];
 
