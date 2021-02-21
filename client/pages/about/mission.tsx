72253ad2ff3a1f3164bae5e55af9a71ea6343aca
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2021-02-20 21:13:39 -0500

    move values to util

--- client/pages/about/mission.tsx
+++ client/pages/about/mission.tsx
@@ -4,6 +4,7 @@ import ImageStrip from '../../components/ImageStrip';
 import Layout from '../../components/layout';
 import ValuesCard from '../../components/ValuesCard';
 import Blockquote from '../../components/Blockquote';
+import { values } from '../../utils/values';
 
 interface ISectionProps {
   background?: string;
@@ -113,44 +114,6 @@ const Cards = styled.div<ICardsProps>`
 `;
 
 export default function Mission() {
-  const values = [
-    {
-      value: 'Smart Work',
-      text:
-        'Finding and delivering the most effective and efficient results without sacrificing quality or integrity.',
-      image: 'olav-ahrens-rotne-4Ennrbj1svk-unsplash.jpg'
-    },
-    {
-      value: 'Professionalism',
-      text:
-        'Using our in-depth knowledge, industry knowledge and expertise to serve you. We are industry expert, and We take ownership of our work.',
-      image: 'hunters-race-MYbhN8KaaEc-unsplash.jpg'
-    },
-    {
-      value: 'Responsiveness',
-      text: 'We take ownership of our work and deliver timely results.',
-      image: 'alizee-baudez-npJkoTc0P8s-unsplash.jpg'
-    },
-    {
-      value: 'Customer Focus',
-      text:
-        'Our solutions are customized for each individual client, candidate and situation',
-      image: 'blake-wisz-0mZIT-EcH5k-unsplash.jpg'
-    },
-    {
-      value: 'Integrity',
-      text:
-        'We do not compromise our values, our clients, or candidates just to close a deal. We believe in honest and open communication that include speaking the fact-based truths, even when it’s hard to hear it.',
-      image: 'brett-jordan-3h6Q-9Y0LqI-unsplash.jpg'
-    },
-    {
-      value: 'Respect',
-      text:
-        'We value our clients and candidates trust and confidence. We will safeguard your privacy and not waste your time.',
-      image: 'tiago-felipe-ferreira-saJkxOZXPsk-unsplash.jpg'
-    }
-  ];
-
   return (
     <Layout page='Our Mission'>
       <section>
---
+++ client/utils/values.ts
@@ -0,0 +1,37 @@
+export const values = [
+  {
+    value: 'Smart Work',
+    text:
+      'Finding and delivering the most effective and efficient results without sacrificing quality or integrity.',
+    image: 'olav-ahrens-rotne-4Ennrbj1svk-unsplash.jpg'
+  },
+  {
+    value: 'Professionalism',
+    text:
+      'Using our in-depth knowledge, industry knowledge and expertise to serve you. We are industry expert, and We take ownership of our work.',
+    image: 'hunters-race-MYbhN8KaaEc-unsplash.jpg'
+  },
+  {
+    value: 'Responsiveness',
+    text: 'We take ownership of our work and deliver timely results.',
+    image: 'alizee-baudez-npJkoTc0P8s-unsplash.jpg'
+  },
+  {
+    value: 'Customer Focus',
+    text:
+      'Our solutions are customized for each individual client, candidate and situation',
+    image: 'blake-wisz-0mZIT-EcH5k-unsplash.jpg'
+  },
+  {
+    value: 'Integrity',
+    text:
+      'We do not compromise our values, our clients, or candidates just to close a deal. We believe in honest and open communication that include speaking the fact-based truths, even when it’s hard to hear it.',
+    image: 'brett-jordan-3h6Q-9Y0LqI-unsplash.jpg'
+  },
+  {
+    value: 'Respect',
+    text:
+      'We value our clients and candidates trust and confidence. We will safeguard your privacy and not waste your time.',
+    image: 'tiago-felipe-ferreira-saJkxOZXPsk-unsplash.jpg'
+  }
+];
