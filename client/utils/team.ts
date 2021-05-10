ea9691258402f1008475c50b8550e4eec1525908
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2021-05-09 20:56:58 -0400

    add Michelle to team

--- client/utils/team.ts
+++ client/utils/team.ts
@@ -1,6 +1,16 @@
 export const team = [
   {
     id: 1,
+    name: 'Michelle Goltman',
+    title: 'Partner & Director of Business Development',
+    image: '/michelle-goltman.jpg',
+    phone: '5877602432',
+    email: 'Michelle.Goltman@tritan.ca',
+    bio:
+      'Michelle is an experienced recruitment consultant with a background in construction, Oil and gas, supply chain, manufacturing and skilled trades. She spent more than 12+ years with a couple of Canada’s largest recruiting agencies before joining TRITAN Group. As a partner Michelle is direct and honest to a fault.  Her secret weapon is her ability to connect with anyone like she’s known them for years. '
+  },
+  {
+    id: 2,
     name: 'Hailey Mumby',
     title: 'Director of business Development',
     image: '/hailey-mumby.jpg',
@@ -10,7 +20,7 @@ export const team = [
       'After she has spent the nearly 10 years working with two of the largest third-party recruitment agencies in Canada, Hailey decided to start her own firm TRITAN  Group. Using her experience, she created an agency that is focused on building customized solution for clients, especially targeted senior level roles. Hailey is an expert in  Construction, Property Management,  Real Estate, office professionals and skilled trades. She is a graduate of St Mary’s University with a double major in finance and marketing with an HR certificate.'
   },
   {
-    id: 2,
+    id: 3,
     name: 'Rebecca Bolsover',
     title: 'Recruiter',
     image: '/rebecca-bolsover.jpg',
