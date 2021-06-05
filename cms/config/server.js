f5358b507618fef8d97d51b7b0c486b13ebb0496
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2021-06-04 20:46:27 -0400

    add strapi cms and docker pg config

---
+++ cms/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '13f903d05c5b17bd071782f77e9e2f17'),
+    },
+  },
+});
