4cf8b5bedf890556b3bd5728f9f67a4dc837eb35
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2021-06-12 16:23:24 -0400

    setup cms for deployment

--- cms/config/database.js
+++ cms/config/database.js
@@ -1,18 +1,24 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "bookshelf",
       settings: {
-        client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5544),
-        database: env('DATABASE_NAME', 'tritan'),
-        username: env('DATABASE_USERNAME', 'postgres'),
-        password: env('DATABASE_PASSWORD', 'postgres'),
-        ssl: env.bool('DATABASE_SSL', false),
+        client: "postgres",
+        host: env("DATABASE_HOST", "127.0.0.1"),
+        port: env.int("DATABASE_PORT", 5544),
+        database: env("DATABASE_NAME", "tritan"),
+        username: env("DATABASE_USERNAME", "postgres"),
+        password: env("DATABASE_PASSWORD", "postgres"),
+        // ssl: env.bool('DATABASE_SSL', false),
+        ssl:
+          process.env.NODE_ENV === "development"
+            ? env.bool("DATABASE_SSL", false)
+            : {
+                rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+              },
       },
-      options: {}
+      options: {},
     },
   },
 });
