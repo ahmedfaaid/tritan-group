32e307a1fcb79578aee749b5b10447ec439d04c9
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2021-10-27 18:58:08 -0400

    Update team bios

--- client/utils/team.ts
+++ client/utils/team.ts
@@ -6,7 +6,7 @@ export const team = [
     image: '/hailey-mumby.jpg',
     phone: '5879857609',
     email: 'Hailey.Mumby@tritan.ca',
-    bio: 'After she has spent the nearly 10 years working with two of the largest third-party recruitment agencies in Canada, Hailey decided to start her own firm TRITAN  Group. Using her experience, she created an agency that is focused on building customized solution for clients, especially targeted senior level roles. Hailey is an expert in  Construction, Property Management,  Real Estate, office professionals and skilled trades. She is a graduate of St Mary’s University with a double major in finance and marketing with an HR certificate.'
+    bio: 'After spending nearly 10 years working with two of the largest third-party recruitment agencies in Canada, Hailey decided to start her own firm, TRITAN Group. Using her experience, she created an agency that is focused on building customized solution for clients; especially targeted senior level roles. Hailey is an expert in Construction, Property Management, Real Estate, office professionals and skilled trades. She is a graduate of St Mary’s University with a double major in finance and marketing with an HR certificate.'
   },
   {
     id: 2,
@@ -15,6 +15,6 @@ export const team = [
     image: '/rebecca-bolsover.jpg',
     phone: '5877602430',
     email: 'Rebecca.Bolsover@tritan.ca',
-    bio: 'Rebecca is a global citizen. She spent the past 12 years has living and working in South Korea, Kenya, Hong Kong and Bermuda. In that time, she developed a deep understanding for working in and with a diverse workforce. Rebecca has a Bachelors’ from Saint Mary’s University and a Post Graduate Certificate in Human Resources Management. She is currently preparing to write the NKE in 2021. In recruitment, she is diligent and delivers exceptional results.  In her spare time, Rebecca can be found enjoying a book or spending time along the Northumberland Strait coastline with her family.'
+    bio: 'Rebecca is a global citizen. She spent the past 12 years living and working in South Korea, Kenya, Hong Kong and Bermuda. In that time, she developed a deep understanding for working within and around a diverse workforce. Rebecca has a Bachelors’ from Saint Mary’s University and a Post Graduate Certificate in Human Resources Management. She is currently preparing to write the NKE in 2021. In recruitment, she is diligent and delivers exceptional results. In her spare time, Rebecca can be found enjoying a book or spending time along the Northumberland Strait coastline with her family.'
   }
 ];
