82c5c676e6de4c18abce51243f995b3c44b6a582
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2022-10-12 16:26:23 -0400

    update config folder

--- cms/config/database.js
+++ cms/config/database.js
@@ -1,24 +1,13 @@
-module.exports = ({ env }) => ({
-  defaultConnection: "default",
-  connections: {
-    default: {
-      connector: "bookshelf",
-      settings: {
-        client: "postgres",
-        host: env("DATABASE_HOST", "127.0.0.1"),
-        port: env.int("DATABASE_PORT", 5544),
-        database: env("DATABASE_NAME", "tritan"),
-        username: env("DATABASE_USERNAME", "postgres"),
-        password: env("DATABASE_PASSWORD", "postgres"),
-        // ssl: env.bool('DATABASE_SSL', false),
-        ssl:
-          process.env.NODE_ENV === "development"
-            ? env.bool("DATABASE_SSL", false)
-            : {
-                rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
-              },
-      },
-      options: {},
+export default ({ env }) => ({
+  connection: {
+    client: "postgres",
+    connection: {
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 5544),
+      database: env("DATABASE_NAME", "drop-it"),
+      user: env("DATABASE_USERNAME", "postgres"),
+      password: env("DATABASE_PASSWORD", "postgres"),
+      ssl: env.bool("DATABASE_SSL", false),
     },
   },
 });
