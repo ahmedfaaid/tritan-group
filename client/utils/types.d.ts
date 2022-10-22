b3c09a5acb8af81d888f0680a39462db95af3017
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2022-10-21 23:55:48 -0400

    v4 api response types

--- client/utils/types.d.ts
+++ client/utils/types.d.ts
@@ -1,24 +1,28 @@
 export type Job = {
   id: number;
-  title: string;
-  location: string;
-  summary: string;
-  duties: string;
-  qualifications: string;
-  benefits: string;
-  date: Date;
-  industry: string;
-  contact_name: string;
-  contact_email: string;
-  contact_phone: string;
+  attributes: {
+    title: string;
+    location: string;
+    summary: string;
+    duties: string;
+    qualifications: string;
+    benefits: string;
+    date: Date;
+    industry: string;
+    contact_name: string;
+    contact_email: string;
+    contact_phone: string;
+  };
 };
 
 export type Team = {
   id: number;
-  name: string;
-  title: string;
-  image: any;
-  phone: string;
-  email: string;
-  bio: string;
+  attributes: {
+    name: string;
+    title: string;
+    image: any;
+    phone: string;
+    email: string;
+    bio: string;
+  };
 };
